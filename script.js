ea2b0816cbc9b72ccb977f40f09f64a4de189bdf
christian marchello <marchello.christian@gmail.com>
2023-04-25 16:33:09 -0400

    added teams page, script functions, css

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://api-nba-v1.p.rapidapi.com/seasons',
-	method: 'GET',
-	headers: {
-		'content-type': 'application/octet-stream',
-		'X-RapidAPI-Key': 'c5c7f5acfbmsh559cf33f7adee0dp192e66jsn118bb5513352',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-	}
-};
+async function getData(){
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+        const url = 'https://api-nba-v1.p.rapidapi.com/teams';
+        const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': 'c5c7f5acfbmsh559cf33f7adee0dp192e66jsn118bb5513352',
+            'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function playerData(){
+        const url = 'https://api-nba-v1.p.rapidapi.com/players';
+        const options = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/octet-stream',
+            'X-RapidAPI-Key': 'c5c7f5acfbmsh559cf33f7adee0dp192e66jsn118bb5513352',
+            'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+        
+
+}
 
 
+
+
+
+
+async function mainEvent(){
+    console.log("Start");
+    getData()
+    const mainForm = document.querySelector('.main-form');
+    const teamForms = document.querySelector('#load-teams');
+    let teamList = []
+    teamForms.addEventListener('click,')
+    
+}
+
+document.addEventListener('DOMContentLoaded', async => mainEvent());
 
